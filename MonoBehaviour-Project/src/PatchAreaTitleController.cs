3ec975d70fff3eef5532b7cb73363024fc66b63c
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-08-31 17:44:25 +0200

    add only member variables monobehaviour classes

---
+++ MonoBehaviour-Project/src/PatchAreaTitleController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace MyFirstCustomSceneMod;
+
+class PatchAreaTitleController : MonoBehaviour
+{
+    [Range(0, 10)] public float Pause = 3f;
+    public bool AlwaysVisited = false;
+    public bool DisplayRight = false;
+    public bool OnlyOnRevisit = false;
+    public bool SubArea = true;
+    public bool WaitForTrigger = false;
+    public string AreaEvent = "";
+    public string VisitedBool = "";
+}
---
+++ MonoBehaviour-Project/src/PatchPlayMakerManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace MyFirstCustomSceneMod;
+
+class PatchPlayMakerManager : MonoBehaviour
+{
+    public Transform ManagerTransform;
+}
