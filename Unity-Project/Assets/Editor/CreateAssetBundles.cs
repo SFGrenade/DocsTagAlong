be0c0c613a53fd893fc0f2591a34ff9cf0257fec
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-08-31 18:10:09 +0200

    add editor scripts

---
+++ Unity-Project/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+  [MenuItem("Build AssetBundles/Build AssetBundles Compressed")]
+  static void BuildAllAssetBundlesCompressed()
+  {
+    string assetBundleDirectory = "Assets/AssetBundles";
+    if(!Directory.Exists(assetBundleDirectory))
+    {
+      Directory.CreateDirectory(assetBundleDirectory);
+    }
+    BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+                                    BuildAssetBundleOptions.None, 
+                                    BuildTarget.StandaloneWindows64);
+  }
+  
+  [MenuItem("Build AssetBundles/Build AssetBundles Uncompressed")]
+  static void BuildAllAssetBundlesUncompressed()
+  {
+    string assetBundleDirectory = "Assets/AssetBundles";
+    if(!Directory.Exists(assetBundleDirectory))
+    {
+      Directory.CreateDirectory(assetBundleDirectory);
+    }
+    BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+                                    BuildAssetBundleOptions.UncompressedAssetBundle, 
+                                    BuildTarget.StandaloneWindows64);
+  }
+}
