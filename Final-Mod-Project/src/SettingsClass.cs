2fb53750c867231c8320ffae970506eb78344f67
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-08-31 17:35:28 +0200

    add settings class

---
+++ Final-Mod-Project/src/SettingsClass.cs
@@ -0,0 +1,7 @@
+namespace MyFirstCustomSceneMod;
+
+public class SettingsClass
+{
+    // we name the members this way to make it easier for the Get-/SetPlayerBoolHook
+    public bool MyFirstCustomSceneMod_VisitedArea = false;
+}
