ba24ff1ca70c198fa45c6dc40aad4fa1b663bf20
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-08-31 17:36:26 +0200

    add hooks

--- Final-Mod-Project/src/MyFirstCustomSceneMod.cs
+++ Final-Mod-Project/src/MyFirstCustomSceneMod.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using SFCore.Generics;
+using SFCore.Utils;
 using UnityEngine;
 
 namespace MyFirstCustomSceneMod;
@@ -55,5 +56,83 @@ public class MyFirstCustomSceneMod : SaveSettingsMod<SettingsClass>
         ModHooks.SetPlayerBoolHook += OnSetPlayerBoolHook;
         ModHooks.LanguageGetHook += OnLanguageGetHook;
         UnityEngine.SceneManagement.SceneManager.activeSceneChanged += OnSceneChanged;
+    }
+
+    private bool OnGetPlayerBoolHook(string target, bool orig)
+    {
+        var tmpField = ReflectionHelper.GetFieldInfo(typeof(SettingsClass), target);
+        if (tmpField != null)
+        {
+            return (bool)tmpField.GetValue(SaveSettings);
+        }
+
+        return orig;
+    }
+
+    private bool OnSetPlayerBoolHook(string target, bool orig)
+    {
+        var tmpField = ReflectionHelper.GetFieldInfo(typeof(SettingsClass), target);
+        if (tmpField != null)
+        {
+            tmpField.SetValue(SaveSettings, orig);
+        }
+
+        return orig;
+    }
+
+    private string OnLanguageGetHook(string key, string sheet, string orig)
+    {
+        // this hook can be trivialized using `SFCore.Utils.LanguageStrings`
+        if (sheet == "Titles" && key == "MyFirstCustomSceneMod_AreaTitle_SUPER")
+        {
+            return "The";
+        }
+
+        if (sheet == "Titles" && key == "MyFirstCustomSceneMod_AreaTitle_MAIN")
+        {
+            return "Lands";
+        }
+
+        if (sheet == "Titles" && key == "MyFirstCustomSceneMod_AreaTitle_SUB")
+        {
+            return "Between";
+        }
+
+        return orig;
+    }
+
+    private void OnSceneChanged(UnityEngine.SceneManagement.Scene from, UnityEngine.SceneManagement.Scene to)
+    {
+        if (to.name == "Town")
+        {
+            // we arrived in Dirtmouth
+            // get the transition point in the well
+            var tp1 = to.Find("bot1").GetComponent<TransitionPoint>();
+            // we want to get to our custom scene
+            tp1.targetScene = "MyFirstCustomSceneMod";
+            // we'll enter our custom scene from the left
+            tp1.entryPoint = "left1";
+        }
+        else if (to.name == "Crossroads_01")
+        {
+            // we arrived in the Forgotten Crossroads
+            // get the transition point in the well
+            var tp1 = to.Find("top2").GetComponent<TransitionPoint>();
+            // we want to get to our custom scene
+            tp1.targetScene = "MyFirstCustomSceneMod";
+            // we'll enter our custom scene from the right
+            tp1.entryPoint = "right1";
+
+            var tp2Go = to.Find("door1");
+            // get the transition point at the bottom
+            var tp2 = tp2Go.GetComponent<TransitionPoint>();
+            // we want to get to our custom scene
+            tp2.targetScene = "MyFirstCustomSceneMod";
+            // we'll enter our custom scene from the right
+            tp2.entryPoint = "right1";
+            var tp2Fsm = tp2Go.LocateMyFSM("Door Control");
+            tp2Fsm.GetStringVariable("New Scene").Value = tp2.targetScene;
+            tp2Fsm.GetStringVariable("Entry Gate").Value = tp2.entryPoint;
+        }
     }
 }
